16f27a71f5b8647b7b44194e28f59bdccd6b0db2
Kianlywn <faizbussines86@gmail.com>
2025-05-27 10:58:40 +0700

    Add files via upload

---
+++ MenuAdmin.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PBO_Besar.Menu.Menu_Admin
+{
+    partial class MenuAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnKasir = new Button();
+            btnBack = new Button();
+            SuspendLayout();
+            // 
+            // btnKasir
+            // 
+            btnKasir.Location = new Point(405, 112);
+            btnKasir.Name = "btnKasir";
+            btnKasir.Size = new Size(203, 40);
+            btnKasir.TabIndex = 0;
+            btnKasir.Text = "Data Kasir";
+            btnKasir.UseVisualStyleBackColor = true;
+            btnKasir.Click += btnKasir_Click;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(690, 359);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(203, 40);
+            btnBack.TabIndex = 1;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // MenuAdmin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1022, 479);
+            Controls.Add(btnBack);
+            Controls.Add(btnKasir);
+            Name = "MenuAdmin";
+            Text = "MenuAdmin";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnKasir;
+        private Button btnBack;
+    }
+}
---
+++ MenuAdmin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PBO_Besar.Menu.Menu_Admin.Data_Kasir;
+
+namespace PBO_Besar.Menu.Menu_Admin
+{
+    public partial class MenuAdmin : Form
+    {
+        public MenuAdmin(int userId)
+        {
+            InitializeComponent();
+        }
+
+        private void btnKasir_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            // Buat instance form DataKasir
+            DataKasir dataKasirForm = new DataKasir();
+
+            // Event handler saat form DataKasir ditutup
+            dataKasirForm.FormClosed += (s, args) =>
+            {
+                // Tampilkan kembali form admin/menu ketika form DataKasir ditutup
+                this.Show();
+            };
+
+            // Tampilkan form DataKasir
+            dataKasirForm.Show();
+        }
+    }
+}
